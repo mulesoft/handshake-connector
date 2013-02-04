eeefea6f451d8b2d83b3d8ddf140ca003f47de99
marianosimone <marianosimone@gmail.com>
2013-02-04 11:22:05 -0300

    follow packages convention

--- src/test/java/org/mule/handshake/HandshakeConnectorTest.java
+++ src/test/java/org/mule/handshake/HandshakeConnectorTest.java
@@ -11,15 +11,13 @@
  */
 package org.mule.handshake;
 
+import org.junit.Test;
 import org.mule.api.MuleEvent;
 import org.mule.construct.Flow;
-import org.mule.tck.FunctionalTestCase;
 import org.mule.tck.AbstractMuleTestCase;
+import org.mule.tck.FunctionalTestCase;
 
-import org.junit.Test;
-
-public class HandshakeConnectorTest extends FunctionalTestCase
-{
+public class HandshakeConnectorTest extends FunctionalTestCase {
     @Override
     protected String getConfigResources()
     {
@@ -27,8 +25,7 @@ public class HandshakeConnectorTest extends FunctionalTestCase
     }
 
     @Test
-    public void testFlow() throws Exception
-    {
+    public void testFlow() throws Exception {
         runFlowAndExpect("testFlow", "Another string");
     }
 
@@ -38,8 +35,7 @@ public class HandshakeConnectorTest extends FunctionalTestCase
     * @param flowName The name of the flow to run
     * @param expect The expected output
     */
-    protected <T> void runFlowAndExpect(String flowName, T expect) throws Exception
-    {
+    protected <T> void runFlowAndExpect(String flowName, T expect) throws Exception {
         Flow flow = lookupFlowConstruct(flowName);
         MuleEvent event = AbstractMuleTestCase.getTestEvent(null);
         MuleEvent responseEvent = flow.process(event);
@@ -55,8 +51,7 @@ public class HandshakeConnectorTest extends FunctionalTestCase
     * @param expect The expected output
     * @param payload The payload of the input event
     */
-    protected <T, U> void runFlowWithPayloadAndExpect(String flowName, T expect, U payload) throws Exception
-    {
+    protected <T, U> void runFlowWithPayloadAndExpect(String flowName, T expect, U payload) throws Exception {
         Flow flow = lookupFlowConstruct(flowName);
         MuleEvent event = AbstractMuleTestCase.getTestEvent(payload);
         MuleEvent responseEvent = flow.process(event);
@@ -69,8 +64,7 @@ public class HandshakeConnectorTest extends FunctionalTestCase
      *
      * @param name Name of the flow to retrieve
      */
-    protected Flow lookupFlowConstruct(String name)
-    {
+    protected Flow lookupFlowConstruct(String name) {
         return (Flow) AbstractMuleTestCase.muleContext.getRegistry().lookupFlowConstruct(name);
     }
 }
